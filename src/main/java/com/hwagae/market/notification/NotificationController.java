4367652dcb90917e733471dc4d4090d8be2f3e49
YouGoWoon <rhdnsd409@naver.com>
2024-04-09 02:19:41 +0900

    알림목록 리스트.html 생성완료

--- src/main/java/com/hwagae/market/notification/NotificationController.java
+++ src/main/java/com/hwagae/market/notification/NotificationController.java
@@ -1,15 +1,23 @@
 package com.hwagae.market.notification;
 
+import com.hwagae.market.user.UserDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class NotificationController {
-//
-//    @GetMapping("/notification/list")
-////    public String noticeList(){
-////
-////    }
+
+    @GetMapping("/notification/list")
+    public String noticeList(Model model, HttpSession session ){
+        UserDTO userDTO=(UserDTO) session.getAttribute("user");
+
+        return "";
+
+
+    }
 }
